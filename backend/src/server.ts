ed251528530702c687a416f193a0732c5faa6b25
Saad Sayyed <saadsyed950@gmail.com>
2026-03-31 22:14:01 +0530

    Frontend and Backend TS configured

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = 5000;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.json({ status: 200 });
+});
 
 app.listen(PORT, () => {
   console.log(`Server up and running on PORT: ${PORT}`);
