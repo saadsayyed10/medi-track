65fe0ad533873d384a32c6fab34d68a04690a65b
Saad Sayyed <saadsyed950@gmail.com>
2026-04-05 21:24:21 +0530

    Reset forgotten password service

--- backend/src/api/services/user.service.ts
+++ backend/src/api/services/user.service.ts
@@ -7,6 +7,8 @@ import bcryptjs from "bcryptjs";
 // Import files
 import { type SignUpType, SignInType } from "../../types/user.type";
 import { generateToken } from "../../lib/token";
+import { v4 } from "uuid";
+import { resetMail } from "../../mails/passwords.mail.";
 
 /* Register Patient account service */
 export const signUpPatientService = async (data: SignUpType) => {
@@ -80,4 +82,33 @@ export const patientAccountService = async (userId: string) => {
   });
 
   return { user };
+};
+
+/* Reset password service */
+export const resetPasswordService = async (email: string) => {
+  const existing = await prisma.patient.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  // Email should exist in database to reset password
+  if (!existing) throw new Error("Account does not exist");
+
+  // Converting UUID to a 10 character password
+  const newPassword = v4().slice(0, 10);
+
+  const user = await prisma.patient.update({
+    where: {
+      email,
+    },
+    data: {
+      password: newPassword,
+    },
+  });
+
+  // Send new password to patient's email
+  await resetMail(user.email, user.password);
+
+  return user;
 };
--- backend/src/mails/passwords.mail..ts
+++ backend/src/mails/passwords.mail..ts
@@ -1,17 +1,13 @@
 // backend/src/mails/passwords.mail.ts
 
-// Import library
-import { v4 } from "uuid";
+// Import values from env
+import { ENV } from "../config/env.config";
 
 // Import helper function send emails
-import { ENV } from "../config/env.config";
 import { transporter } from "../lib/nodemailer";
 
 // Mail to reset password
-export const resetMail = async (email: string) => {
-  // Converting UUID to a 10 character password
-  const newPassword = v4().slice(0, 10);
-
+export const resetMail = async (email: string, newPassword: string) => {
   await transporter.sendMail({
     from: ENV.EMAIL_USER,
     to: email,
