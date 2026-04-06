13f8486f105b3d403a52cd600996039cfa261376
Saad Sayyed <saadsyed950@gmail.com>
2026-04-06 22:46:27 +0530

    Added backup model

--- agent/LLM.py
+++ agent/LLM.py
@@ -4,9 +4,16 @@ load_dotenv()
 
 # Instead of ChatGoogleGenerativeAI, use ChatOpenAI
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 # LLM model initialized via OpenRouter
 model = ChatOpenAI(
     model="stepfun/step-3.5-flash:free",
     temperature=0.7,
+)
+
+    # LLM model initialized via gemini
+backupModel = ChatGoogleGenerativeAI(
+        model="gemini-3-flash-preview",
+        temperature=0.7,
 )
