07768afda716737e24897a5f7809073c003af7d2
Saad Sayyed <saadsyed950@gmail.com>
2026-04-07 18:51:44 +0530

    Login API

---
+++ frontend/api/auth.api.ts
@@ -0,0 +1,7 @@
+import { LoginUser } from "@/types/user.types";
+import axios from "axios";
+import { serverURL } from "./apiUrl";
+
+export const loginUserAPI = async (data: LoginUser) => {
+  return await axios.post(`${serverURL}/users/login`, { data });
+};
