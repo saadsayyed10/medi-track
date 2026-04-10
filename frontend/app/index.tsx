0c1c79d1f1649d7fc59e0a887263abdc3a7c3ae0
Saad Sayyed <saadsyed950@gmail.com>
2026-04-10 12:22:23 +0530

    Login frontend

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,13 +1,34 @@
-import { Link } from "expo-router";
+import { useAuth } from "@/hooks/useAuth";
+import { Link, useRouter } from "expo-router";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const { logout, hydrate, token } = useAuth();
+
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await logout();
+    await hydrate();
+    router.replace("/login");
+  };
+
+  useEffect(() => {
+    hydrate();
+  }, [hydrate]);
+
   return (
     <View className="flex-1 justify-center items-center w-full">
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Link href={"/login"}>
         <Text>Login</Text>
       </Link>
+      {token && (
+        <Text className="mt-10 text-xl" onPress={handleLogout}>
+          Logout
+        </Text>
+      )}
     </View>
   );
 }
