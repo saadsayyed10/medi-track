92238a2e28bd9ea8be24eb4eb8f4592b574708e1
Saad Sayyed <saadsyed950@gmail.com>
2026-04-11 16:53:15 +0530

    Patient register type

--- frontend/types/user.types.ts
+++ frontend/types/user.types.ts
@@ -2,3 +2,14 @@ export type LoginUser = {
   email: string;
   password: string;
 };
+
+export type RegisterUser = {
+  name: string;
+  email: string;
+  password: string;
+  age: string;
+  allergies: string;
+  allergiesKeywords: string[];
+  healthIssues: string;
+  healthIssuesKeywords: string[];
+};
