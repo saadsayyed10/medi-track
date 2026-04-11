06eb3468d1439c648341fbc6f3be787856fff692
Saad Sayyed <saadsyed950@gmail.com>
2026-04-11 16:54:35 +0530

    Register patient helper API

--- frontend/api/auth.api.ts
+++ frontend/api/auth.api.ts
@@ -1,7 +1,11 @@
-import { type LoginUser } from "@/types/user.types";
+import { type RegisterUser, type LoginUser } from "@/types/user.types";
 import axios from "axios";
 import { serverURL } from "./apiUrl";
 
 export const loginUserAPI = async (data: LoginUser) => {
   return await axios.post(`${serverURL}/users/login`, data);
+};
+
+export const registerUserAPI = async (data: RegisterUser) => {
+  return await axios.post(`${serverURL}/users/register`, data);
 };
