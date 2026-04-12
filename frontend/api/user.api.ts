fd4f40ddcb889d990633e63798aadc77d49ae15d
Saad Sayyed <saadsyed950@gmail.com>
2026-04-12 16:49:27 +0530

    Delete user API

--- frontend/api/user.api.ts
+++ frontend/api/user.api.ts
@@ -1,13 +1,20 @@
 import { type ChangePassword } from "@/types/user.types";
 import axios from "axios";
 import { serverURL } from "./apiUrl";
-import { useAuth } from "@/hooks/useAuth";
 
 export const changePasswordAPI = async (
   data: ChangePassword,
   token: string,
 ) => {
   return axios.put(`${serverURL}/users/change-password`, data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const deleteAPI = async (token: string) => {
+  return axios.delete(`${serverURL}/users/delete`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
