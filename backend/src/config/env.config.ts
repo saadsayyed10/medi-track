2124a79e464a18bb3a4bf7f3c7c9955faa6a1294
Saad Sayyed <saadsyed950@gmail.com>
2026-04-15 07:46:43 +0530

    Cloudinary helper function

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -7,4 +7,7 @@ export const ENV = {
   JWT_SECRET: process.env.JWT_SECRET || "sameoldtreva",
   EMAIL_USER: process.env.EMAIL_USER,
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+  CLOUDINARY_CLOUD: process.env.CLOUDINARY_CLOUD,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
 };
