55a7d470c341e8e28fca74e2699eadccd0642d9b
Saad Sayyed <saadsyed950@gmail.com>
2026-04-17 22:12:29 +0530

    Business logic upload prescription

--- backend/src/api/services/upload.service.ts
+++ backend/src/api/services/upload.service.ts
@@ -1,6 +1,9 @@
 // backend/src/api/services/upload.service.ts
 
 // Import ORM instance to communicate with database
+import axios from "axios";
+import { ENV } from "../../config/env.config";
+import cloudinary from "../../lib/cloudinary";
 import prisma from "../../lib/prisma.orm";
 
 // Import types
@@ -8,14 +11,29 @@ import { UploadPrescription } from "../../types/upload.type";
 
 /* Upload prescription service */
 export const uploadPrescriptionService = async (data: UploadPrescription) => {
+  const user = await prisma.patients.findUnique({
+    where: {
+      id: data.userId,
+    },
+  });
+  const uploadResponse = await cloudinary.uploader.upload(data.uploadImage);
+  const imageUrl = uploadResponse.secure_url;
+
   const prescription = await prisma.prescriptions.create({
     data: {
-      uploaded_image: data.uploadImage,
+      uploaded_image: imageUrl,
       patients_id: data.userId,
     },
   });
 
-  return prescription;
+  const res = await axios.post(`${ENV.AI_URL}/upload-prescription`, {
+    imageUrl,
+    email: user?.email,
+  });
+
+  const content = res.data.ocr.lines;
+
+  return { content, prescription };
 };
 
 /* Fetch all upload prescriptions service */
